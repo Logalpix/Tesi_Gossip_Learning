f48169e9dcdf8b9b1271f5cd0ee17a0ceb2f7753
Logalpix <lorenzogalass099@gmail.com>
2024-12-16 12:29:17 +0100

    Added support for data persistence via .csv files for test results

--- gossip_learning.js
+++ gossip_learning.js
@@ -18,6 +18,7 @@ import AsyncLock from 'async-lock'
 
 const lock = new AsyncLock()
 const path_dir_models = 'models/'
+const path_dir_results = 'results/'
 var num_send_to_do = 1
 var num_of_known_peers = 0
 
@@ -235,6 +236,11 @@ def test(model, test_loader):
   test_loss /= len(test_loader.dataset)
   acc = correct / len(test_loader.dataset)
 
+  container_id = os.getenv("HOSTNAME")
+  with open(f"results/test_log_{container_id}.csv", "a") as f:
+    f.write(f"{test_loss:.4f},{acc:.4f}\\n")
+    logging.debug(f"Test Loss: {test_loss:.4f}, Accuracy: {acc:.4f}")
+
   return test_loss, acc
 
 
@@ -288,6 +294,10 @@ def create_test_loader():
 
 if (!fs.existsSync(path_dir_models)){
 	fs.mkdirSync(path_dir_models)
+}
+
+if (!fs.existsSync(path_dir_results)){
+	fs.mkdirSync(path_dir_results)
 }
 
 const my_ip = get_ip_addr()
